7fe3aaa00f079847e5457ee0c9fd292c52bda0dc
Henri-Matias Tuomaala <henri-matias.tuomaala@aalto.fi>
2020-01-21 12:45:14 +0200

    Security stuff

--- reaktor_app/reaktor_app/settings.py
+++ reaktor_app/reaktor_app/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rb&f0#2u8j^95*0dd4x%+qim_f*7ml2v^2vscs&@txz8mj)nvl'
+#SECRET_KEY = 'rb&f0#2u8j^95*0dd4x%+qim_f*7ml2v^2vscs&@txz8mj)nvl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
